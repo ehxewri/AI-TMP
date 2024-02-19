60c0782cc348e28f3b7fada10974a38329e437f6
ehxewri <frank.hanan@fjhit.com>
2024-02-18 22:25:08 -0600

    lets get started

---
+++ Create-github-repo.sh
@@ -0,0 +1,7 @@
+curl -X POST \
+  -H "Authorization: token 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" \
+  -H "Content-Type: application/json" \
+  -d '{"name": "AI-TMP", "description": "AI-Playground", "private": false}' \
+  https://api.github.com/user/repos
+git remote add origin https://github.com/ehxewri/"repo-Name"
+git push -u origin main
