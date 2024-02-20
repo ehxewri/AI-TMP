185d242e47350bc79c27a476a7e901ac9aeb7ad6
ehxewri <frank.hanan@fjhit.com>
2024-02-19 20:19:09 -0600

    updated repo and key

--- Create-github-repo.sh
+++ Create-github-repo.sh
@@ -1,7 +1,7 @@
 curl -X POST \
-  -H "Authorization: token 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" \
+  -H "Authorization: token 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 " \
   -H "Content-Type: application/json" \
-  -d '{"name": "AI-TMP", "description": "AI-Playground", "private": false}' \
+  -d '{"name": "ai-case-study", "description": "Case study for Cisco IBN", "private": false}' \
   https://api.github.com/user/repos
-git remote add origin https://github.com/ehxewri/"repo-Name"
-git push -u origin main
+# git remote add origin https://github.com/ehxewri/"repo-Name"
+# git push -u origin main
